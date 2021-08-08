26d6248121d3cf7baa9b8b92cff04d3807af7dce
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-08-08 13:18:26 +0100

    questions added to index.js

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 
+const inquirer = require('inquirer');
+const util =require("util");
+const fs = require("fs")
 
+
+  .prompt([
+   
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "Unlicense",
+            "Apache 2.0",
+            "GNU v3",
+            "BSD 3-Clause",
+            "Mozilla Public License 2.0"
+        ]
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repository?",
+        name: "contribute"
+    },
+
+   
+]
+   
+    /* Pass your questions in here */
+  )
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 
 
